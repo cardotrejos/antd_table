b114640ce03cce4b5c88c1d2d80cae9ce2de0c4e
cardotrejos <cardotrejos@gmail.com>
2020-04-16 20:51:53 -0500

    Updates

---
+++ precache-manifest.57b1164cdb9dc7521603654e92c408e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2a712086e27cf0e668a3544d5147eca",
+    "url": "/antd_table/index.html"
+  },
+  {
+    "revision": "8fa7e62ad62332ef73b5",
+    "url": "/antd_table/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "54cf2a3ba8896d99b34e",
+    "url": "/antd_table/static/css/main.aa4dabfa.chunk.css"
+  },
+  {
+    "revision": "8fa7e62ad62332ef73b5",
+    "url": "/antd_table/static/js/2.616bbee0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/antd_table/static/js/2.616bbee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cf2a3ba8896d99b34e",
+    "url": "/antd_table/static/js/main.040835c0.chunk.js"
+  },
+  {
+    "revision": "d5ce4d0e4c44811b0023",
+    "url": "/antd_table/static/js/runtime-main.53afa161.js"
+  }
+]);
