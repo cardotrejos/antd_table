0240427891034c1d3652574a7da775febc93fd75
Ricardo <cardotrejos@gmai..com>
2020-04-16 22:36:08 -0500

    Style CSS with layout and add logo
    Adding some style to layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, {Component} from 'react';
-import './App.css';
 
 import DataPage from './pages/DataPage'
+import 'antd/dist/antd.css';
+import './index.css';
+import logo from './logo.png'
+import { Layout } from 'antd';
+
+const { Header, Content, Footer } = Layout;
 
 class App extends Component {
     render() {
     return (
-      <div className="App">
-        <h1>Summation of all fuels used for electricity generation</h1>
+      <Layout className="layout">
+    <Header>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <div className="site-layout-content">
+        <h1>Tabla generación de energía</h1>
         <DataPage />
       </div>
+    </Content>
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ricardo Trejos</Footer>
+  </Layout>
+
     );
   }
 }
